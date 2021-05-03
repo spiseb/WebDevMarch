49968dd85eef6f0171055cb9a72f80a784440030
injecti0n <9992685+injecti0n@users.noreply.github.com>
2021-05-03 10:20:04 +0200

    updated

---
+++ JAVASCRIPT/tutorial1/app.js
@@ -0,0 +1,89 @@
+console.log('hello 1 from app.js');
+
+document.getElementById('intecbrussel').style.backgroundColor='#000000'
+
+document.getElementById('intecbrussel').style.display="none"
+
+document.getElementById('intecbrussel').innerHTML="<h1>intecbrussel</h1>"
+
+console.log("result",document.getElementById('intecbrussel').className)
+
+
+document.querySelector('button').addEventListener('click',(e)=>{
+    alert(1)
+    document.getElementById('intecbrussel').style.display="block"
+})
+
+
+function runpayload(){
+    alert("It's a function")
+}
+
+// runpayload();
+
+document.getElementById('math').innerHTML=(50-5)-(3-4)*(4+4)
+
+
+// JS statements
+
+var x,y,z;
+
+x= 1
+
+// var intecbrussel                   =                                         "test"
+
+console.log(x,y,z,intecbrussel)
+
+
+var name = "Ahmet                                                        "
+console.log(name)
+
+
+var lastname =
+"Mehmet                                                                        ";
+
+console.log(lastname)
+
+
+function X (){
+    // code blocks
+alert(1)
+alert(2)
+}
+// X();
+
+
+
+// var continue = 'test';
+
+
+// console.log(continue)
+
+var ısparta =" test"
+
+console.log(ısparta)
+
+var ist_ank = "test";
+
+// var ISTANBUL = "TEST"
+
+var _ist = "test"
+
+var ist_ = "test";
+
+var $ist="test"
+
+console.log("xxxx",$ist)
+
+// var 34ist = "test";
+
+var ist34 = "test";
+
+// ISTANBUL example
+
+var istanbul = "ist,test1"
+var ISTANBUL = "IST,test2"
+var ISTaNBUL = "IST3,test3"
+
+
+console.log(istanbul,ISTANBUL,ISTaNBUL)
