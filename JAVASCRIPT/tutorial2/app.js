45d758e9f37f04a9edb11caac74f108098b75a83
injecti0n <9992685+injecti0n@users.noreply.github.com>
2021-05-04 11:48:11 +0200

    updated

---
+++ JAVASCRIPT/tutorial2/app.js
@@ -0,0 +1,53 @@
+// let > type of variable
+
+let fullname = "intecbrussel";
+//let fullname = "batman"; re-dec is not allowed 
+
+fullname="Batman" // update is fine
+
+console.log(fullname)
+
+/*
+var = > re-dec and update
+let = > only update
+const = > no update no re-dec
+*/
+
+// const
+
+const carModel = "Audi";
+
+//const carModel ="BMW"
+
+// carModel = "Mercedez"
+
+console.log(carModel)
+
+
+const logicValue = true; //
+
+let test = logicValue;
+
+console.log(logicValue)
+
+test= false
+
+test = "test"
+
+test = {}
+test = ""
+test = []
+test = function(){return 'intec'}
+
+console.log(test)
+console.log(typeof test)
+
+var x ="";
+
+x = "twitter"
+
+var x = "facebook"
+
+console.log(x)
+
+
---
+++ JAVASCRIPT/tutorial3/operators.js
@@ -0,0 +1,59 @@
+// Addition +
+
+let n1 = 1;
+let n2 = 2;
+
+let result = n1 + n2
+result = n1 +=5;
+result = n1 =+10;
+n1 = n1+100;
+result = n1
+
+let name = "Bat";
+name +="Man"
+console.log(name)
+
+// subtraction -
+
+let number1 = 100;
+let number2= 33;
+
+result = number1 - number2;
+// number1 -= number2 // >> number1 = number1 - number2
+// number2 =-number1
+// result = number2;
+
+let NumberInString = "54";
+
+result = Number(NumberInString)+10
+
+result = Number('100') + Number('44');
+
+
+// Division operator >>>>> "/"
+
+result = number1/number2;
+
+
+
+// increment operator "++"
+
+let score = 10;
+score++ // 11
+result = score;
+result++ // 12
+
+++result // 13
+
+// decrement operator "--"
+
+--result // 12
+result-- // 11
+
+
+console.log("Result:", result)
+// console.log("Result:", number1)
+
+
+// FIND AVERAGE SPEED OF AN AUTO WITH JS 
+// USE GOOGLE GET FORMULE
