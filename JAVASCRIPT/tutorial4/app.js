ebb5631af8ae00d60d6c5ea8a358556e4883315b
injecti0n <9992685+injecti0n@users.noreply.github.com>
2021-05-05 20:45:31 +0200

    updated

---
+++ JAVASCRIPT/tutorial4/app.js
@@ -0,0 +1,37 @@
+let number = "1"; // string
+let number1 =  1; // number
+
+let result = number == number1;
+result = number === number1; // variable type and variable value
+
+let n1 = 8;
+let n2 = 7;
+
+result = n1 != n2; // check variable value only
+result = n1 !== Number(n2); // check data type and value
+
+
+// greater than operator ">"
+result = n1 > n2;
+
+n1 = 12
+n2 = 11;
+
+result = n2 < n1; // 10 is less than 1 
+// less than operator
+
+// greter than or equal to next variable ">="
+
+result = n1>=n2; // true;
+
+// less than or equal to next variable "<="
+
+result = n1<=n2;
+
+let str1= "intecbrussel";
+let str2= "xxxxxxxxxxxx";
+
+result = str1 >= str2;
+
+console.log("Result> ",result) //
+
---
+++ JAVASCRIPT/tutorial4/apps.js
@@ -0,0 +1,1 @@
+intecbrussel.innerHTML="intecbrussel";
