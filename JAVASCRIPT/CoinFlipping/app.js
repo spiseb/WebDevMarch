5dd859f602afd25d1fca3423f9d9fd733f1534c6
injecti0n <9992685+injecti0n@users.noreply.github.com>
2021-06-04 23:35:50 +0200

    fixed

--- JAVASCRIPT/CoinFlipping/app.js
+++ JAVASCRIPT/CoinFlipping/app.js
@@ -3,8 +3,9 @@ coin.addEventListener('click', function () {
     coin.classList.remove('heads')
     coin.classList.remove('tails')
     const result = Math.random() < 0.5 ? 'heads' : 'tails';
+    console.log(result)
     setTimeout(function () {
-        if (result) {
+        if (result=='heads') {
             coin.classList.add('heads')
         } else {
             coin.classList.add('tails')
