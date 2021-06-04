c65644f409deb80b85838f2f69aa14aadb3a6156
injecti0n <9992685+injecti0n@users.noreply.github.com>
2021-06-04 16:36:36 +0200

    updated

--- JAVASCRIPT/CoinFlipping/app.js
+++ JAVASCRIPT/CoinFlipping/app.js
@@ -1,4 +1,5 @@
-document.querySelector('#oneEuro').addEventListener('click', function () {
+let coin = document.querySelector('#oneEuro')
+coin.addEventListener('click', function () {
     coin.classList.remove('heads')
     coin.classList.remove('tails')
     const result = Math.random() < 0.5 ? 'heads' : 'tails';
